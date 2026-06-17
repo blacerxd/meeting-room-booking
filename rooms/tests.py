e66f475d0a1eee0f25a123258ddbae243e3d1f98
blacerxd <blacerx657@gmail.com>
2026-06-17 13:07:38 +0300

    test

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+from rooms.models import Room
+
+
+class RoomModelTest(TestCase):
+
+    def test_room_creation(self):
+        room = Room.objects.create(
+            name="Переговорная №1",
+            capacity=10
+        )
+
+        self.assertEqual(room.name, "Переговорная №1")
